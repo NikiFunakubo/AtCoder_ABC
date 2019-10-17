4a04cd27905048831e11b4fc93d18fa57d4ba3c4
NIki Funakubo <kbkbfnkb@gmail.com>
2019-10-18 01:19:40 +0900

    20191018 ABC_C 2問挑戦

---
+++ ABC_practice/src/ABC_081/C/Main.java
@@ -0,0 +1,29 @@
+package ABC_081.C;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    Integer[] num = new Integer[N];
+    
+    for(int i =0; i < N; i++){
+      num[i] = 0;
+    }
+    for(int i = 0; i < N; i++) {
+      num[sc.nextInt() - 1]++;
+    }
+    Arrays.sort(num);    
+    int ans = 0;
+    if( N-K > 0){
+      for(int i = 0; i < N-K ; i++){
+        ans += num[i];
+      }
+    }
+    System.out.println(ans);
+  }
+}
+
